8cd190ba7a04d9843f49ac630da697d9d5115b46
Anthony <anthony.tp1606@gmail.com>
2023-11-27 16:26:36 -0500

    fix test: Corrigiendo los test de ProgressHistoryCanGetDataTest

--- tests/Feature/ProgressHistory/ProgressHistoryCanGetDataTest.php
+++ tests/Feature/ProgressHistory/ProgressHistoryCanGetDataTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\ProgressHistory;
 
 use App\Models\ProgressHistory;
 use App\Models\Recourse;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,38 +12,40 @@ use Tests\TestCase;
 
 class ProgressHistoryCanGetDataTest extends TestCase
 {
-    use RefreshDatabase;
+  use RefreshDatabase;
 
-    /** @test **/
-    public function get_all_progress_history_from_a_recourse_exists(){
-        $recourse = Recourse::factory()->create();
-        $progressHistories = ProgressHistory::factory(4)->create([
-            "recourse_id" => $recourse->id
-        ]);
+  /** @test **/
+  public function get_all_progress_history_from_a_recourse_exists()
+  {
+    $user = User::factory()->create();
+    $recourse = Recourse::factory()->create(["user_id" => $user->id]);
+    $progressHistories = ProgressHistory::factory(4)->create([
+      "recourse_id" => $recourse->id
+    ]);
 
-        $response = $this->getJson(route('progress.index', $recourse));
+    $response = $this->actingAs($user)->getJson(route('progress.index', $recourse));
 
-        $response->assertStatus(Response::HTTP_OK);
-        // Tener en cuenta que puede variar segun la cantidad de resultados por pagina en ApiResponser
-        $response->assertJsonCount(5, "data");
-    }
+    $response->assertStatus(Response::HTTP_OK);
+    // Tener en cuenta que puede variar segun la cantidad de resultados por pagina en ApiResponser
+    $response->assertJsonCount(5, "data");
+  }
 
-    /** @test **/
-    public function can_not_get_progress_history_from_a_recourse_that_not_exists(){
-        $recourse = Recourse::factory()->create();
-        $progressHistories = ProgressHistory::factory(4)->create([
-            "recourse_id" => $recourse->id
-        ]);
+  /** @test **/
+  public function can_not_get_progress_history_from_a_recourse_that_not_exists()
+  {
+    $user = User::factory()->create();
+    $recourse = Recourse::factory()->create(["user_id" => $user->id]);
+    $progressHistories = ProgressHistory::factory(4)->create([
+      "recourse_id" => $recourse->id
+    ]);
 
-        $response = $this->getJson(route('progress.index', 152));
+    $response = $this->actingAs($user)->getJson(route('progress.index', 152));
 
-        $response->assertStatus(Response::HTTP_NOT_FOUND);
-        $response->assertJsonStructure([
-            "error"=>[
-                [
-                    "status", "detail"
-                ]
-            ]
-        ]);
-    }
+    $response->assertStatus(Response::HTTP_NOT_FOUND);
+    $response->assertJsonStructure([
+      "error" => [
+        "status", "detail"
+      ]
+    ]);
+  }
 }
