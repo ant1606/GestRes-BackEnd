303468942f67b206ab7577ea3aec59ac2051e342
Anthony <anthony.tp1606@gmail.com>
2023-11-28 18:48:37 -0500

    fix test: Corrigiendo Tests de StatusDeleteTest

--- tests/Feature/StatusHistory/StatusDeleteTest.php
+++ tests/Feature/StatusHistory/StatusDeleteTest.php
@@ -4,96 +4,92 @@ namespace Tests\Feature\StatusHistory;
 
 use App\Models\Recourse;
 use App\Models\StatusHistory;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class StatusDeleteTest extends TestCase
 {
-    use RefreshDatabase;
+  use RefreshDatabase;
 
-    /** @test **/
-    public function a_status_can_be_deleted_if_is_the_last_register()
-    {
-        $recourse = Recourse::factory()->create();
-        $status1 = StatusHistory::factory()->create([
-            "recourse_id" => $recourse->id,
-            "comment" => "Status Test Second Delete"
-        ]);
-        $status2 = StatusHistory::factory()->create([
-            "recourse_id" => $recourse->id,
-            "comment" => "Status Test Last Delete"
-        ]);
+  /** @test **/
+  public function a_status_can_be_deleted_if_is_the_last_register()
+  {
+    $user = User::factory()->create();
+    $recourse = Recourse::factory()->create(["user_id" => $user->id]);
+    $status1 = StatusHistory::factory()->create([
+      "recourse_id" => $recourse->id,
+      "comment" => "Status Test Second Delete"
+    ]);
+    $status2 = StatusHistory::factory()->create([
+      "recourse_id" => $recourse->id,
+      "comment" => "Status Test Last Delete"
+    ]);
 
-        $this->assertDatabaseCount("status_histories", 3);
+    $this->assertDatabaseCount("status_histories", 3);
 
-        $response = $this->deleteJson(route('status.destroy',  $status2));
+    $response = $this->actingAs($user)->deleteJson(route('status.destroy',  $status2));
 
-        $response->assertStatus(Response::HTTP_ACCEPTED);
-        $this->assertDatabaseCount("status_histories", 2);
-        $response->assertJsonStructure([
-            "data" => [
-                "identificador",
-                "fecha" ,
-                "comentario",
-                "estadoId",
-                "estadoNombre",
-            ]
-        ]);
-    }
+    $response->assertStatus(Response::HTTP_ACCEPTED);
+    $this->assertDatabaseCount("status_histories", 2);
+    $response->assertJsonStructure([
+      "data" => [
+        "identificador",
+        "fecha",
+        "comentario",
+        "estadoId",
+        "estadoNombre",
+      ]
+    ]);
+  }
 
-    /** @test **/
-    public function a_status_can_not_be_deleted_if_is_not_the_last_register()
-    {
-        $this->withoutExceptionHandling();
+  /** @test **/
+  public function a_status_can_not_be_deleted_if_is_not_the_last_register()
+  {
+    $user = User::factory()->create();
+    $recourse = Recourse::factory()->create(["user_id" => $user->id]);
+    $status1 = StatusHistory::factory()->create([
+      "recourse_id" => $recourse->id,
+      "comment" => "Status Test Middle Register"
+    ]);
+    $status2 = StatusHistory::factory()->create([
+      "recourse_id" => $recourse->id,
+      "comment" => "Status Test Last Register"
+    ]);
 
-        $recourse = Recourse::factory()->create();
-        $status1 = StatusHistory::factory()->create([
-            "recourse_id" => $recourse->id,
-            "comment" => "Status Test Middle Register"
-        ]);
-        $status2 = StatusHistory::factory()->create([
-            "recourse_id" => $recourse->id,
-            "comment" => "Status Test Last Register"
-        ]);
+    $this->assertDatabaseCount("status_histories", 3);
 
-        $this->assertDatabaseCount("status_histories", 3);
+    $response = $this->actingAs($user)->deleteJson(route('status.destroy',  $status1));
 
-        $response = $this->deleteJson(route('status.destroy',  $status1));
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
-        $this->assertDatabaseCount("status_histories", 3);
-        $response->assertJsonStructure([
-            "error" => [
-                [
-                    "status",
-                    "detail"
-                ]
-            ]
-        ]);
-    }
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    $this->assertDatabaseCount("status_histories", 3);
+    $response->assertJsonStructure([
+      "error" => [
+        "status",
+        "detail"
+      ]
+    ]);
+  }
 
 
   /** @test **/
   public function a_status_can_not_be_deleted_if_is_generated_by_system()
   {
-    $this->withoutExceptionHandling();
-
-    $recourse = Recourse::factory()->create();
+    $user = User::factory()->create();
+    $recourse = Recourse::factory()->create(["user_id" => $user->id]);
     $statusGenerated = $recourse->status->first();
 
     $this->assertDatabaseCount("status_histories", 1);
 
-    $response = $this->deleteJson(route('status.destroy',  $statusGenerated));
+    $response = $this->actingAs($user)->deleteJson(route('status.destroy',  $statusGenerated));
 
     $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
     $this->assertDatabaseCount("status_histories", 1);
     $response->assertJsonStructure([
       "error" => [
-        [
-          "status",
-          "detail"
-        ]
+        "status",
+        "detail"
       ]
     ]);
   }
