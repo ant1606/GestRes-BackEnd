56af6fe64bff130689605cfb184eecd04797b187
Anthony <anthony.tp1606@gmail.com>
2023-11-28 18:58:38 -0500

    fix test: Corrigiendo Tests de TagRegistrationTest

--- tests/Feature/Tags/TagRegistrationTest.php
+++ tests/Feature/Tags/TagRegistrationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Tags;
 
 use App\Models\Tag;
+use App\Models\User;
 use Tests\TestCase;
 use Illuminate\Support\Str;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,86 +12,82 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TagRegistrationTest extends TestCase
 {
-    use RefreshDatabase;
+  use RefreshDatabase;
 
-    /** @test */
-    public function tag_can_be_register()
-    {
-        // $this->withoutExceptionHandling();
+  /** @test */
+  public function tag_can_be_register()
+  {
+    // $this->withoutExceptionHandling();
+    $user = User::factory()->create();
+    $tag = [
+      "name" => "Etiqueta de prueba"
+    ];
 
-        $tag = [
-            "nombre" => "Etiqueta de prueba"
-        ];
+    $response = $this->actingAs($user)->postJson(route("tag.store"), $tag);
 
-        $response = $this->postJson(route("tag.store"), $tag);
+    $response->assertStatus(Response::HTTP_CREATED);
+    // dd($response->getContent());
+    $this->assertDatabaseCount("tags", 1);
+    $this->assertDatabaseHas("tags", [
+      "name" => Str::upper($tag['name']),
+    ]);
+  }
 
-        $response->assertStatus(Response::HTTP_CREATED);
-        // dd($response->getContent());
-        $this->assertDatabaseCount("tags", 1);
-        $this->assertDatabaseHas("tags", [
-            "name" => Str::upper($tag['nombre']),
-        ]);
-    }
+  /** @test */
+  public function tag_cannot_register_with_empty_name()
+  {
+    // $this->withoutExceptionHandling();
+    $user = User::factory()->create();
+    $tag = [
+      "name" => "",
+      "style" => "bg-gray-700",
+    ];
 
-    /** @test */
-    public function tag_cannot_register_with_empty_name()
-    {
-        // $this->withoutExceptionHandling();
+    $response = $this->actingAs($user)->postJson(route("tag.store"), $tag);
 
-        $tag = [
-            "nombre" => "",
-            "estilos" => "bg-gray-700",
-        ];
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
 
-        $response = $this->postJson(route("tag.store"), $tag);
+    $this->assertDatabaseCount('tags', 0);
 
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    $response->assertJsonStructure([
+      'error' => [
+        "status",
+        "detail"
+      ]
+    ]);
+  }
 
-        $this->assertDatabaseCount('tags', 0);
+  /** @test */
+  public function tag_cannot_register_when_exists_in_database()
+  {
+    // $this->withoutExceptionHandling();
+    $user = User::factory()->create();
+    $tagDuplicated = [
+      "name" => Str::upper("mi etiqueta"),
+      "style" => "bg-gray-700",
+    ];
 
-        $response->assertJsonStructure([
-            'error' => [
-                [
-                    "status",
-                    "detail"
-                ]
-            ]
-        ]);
-    }
+    Tag::factory()->create(["name" => $tagDuplicated["name"], "style" => $tagDuplicated["style"]]);
 
-    /** @test */
-    public function tag_cannot_register_when_exists_in_database()
-    {
-        // $this->withoutExceptionHandling();
+    $this->assertDatabaseCount("tags", 1);
 
-        $tagDuplicated = [
-            "nombre" => Str::upper("mi etiqueta"),
-            "estilos" => "bg-gray-700",
-        ];
+    $response = $this->actingAs($user)->postJson(route("tag.store"), $tagDuplicated);
+    // dd($response->getContent());
 
-        Tag::factory()->create(["name"=>$tagDuplicated["nombre"], "style" => $tagDuplicated["estilos"]]);
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
 
-        $this->assertDatabaseCount("tags", 1);
+    $response->assertJsonStructure([
+      'error' => [
+        "status",
+        "detail"
+      ]
+    ]);
 
-        $response = $this->postJson(route("tag.store"), $tagDuplicated);
-        // dd($response->getContent());
+    $this->assertDatabaseCount("tags", 1);
 
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
-
-        $response->assertJsonStructure([
-            'error' => [
-                [
-                    "status",
-                    "detail"
-                ]
-            ]
-        ]);
-
-        $this->assertDatabaseCount("tags", 1);
-
-        $this->assertDatabaseHas("tags", [
-            "name" => Str::upper($tagDuplicated['nombre']),
-            "style" => "bg-gray-700",
-        ]);
-    }
+    $this->assertDatabaseHas("tags", [
+      "name" => Str::upper($tagDuplicated['name']),
+      "style" => "bg-gray-700",
+    ]);
+  }
 }
