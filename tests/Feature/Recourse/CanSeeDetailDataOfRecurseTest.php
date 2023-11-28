aafcc61565a1dc32906d7de4fb58274403b2e949
Anthony <anthony.tp1606@gmail.com>
2023-11-28 16:09:13 -0500

    fix test: Corrigiendo los test de CanSeeDetailDataOfRecurseTest

--- tests/Feature/Recourse/CanSeeDetailDataOfRecurseTest.php
+++ tests/Feature/Recourse/CanSeeDetailDataOfRecurseTest.php
@@ -5,76 +5,78 @@ namespace Tests\Feature\Recourse;
 use Tests\TestCase;
 use App\Models\Recourse;
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CanSeeDetailDataOfRecurseTest extends TestCase
 {
-    use RefreshDatabase;
+  use RefreshDatabase;
 
-    /** @test */
-    public function get_detailed_data_of_recourse()
-    {
-        // $this->withoutExceptionHandling();
+  /** @test */
+  public function get_detailed_data_of_recourse()
+  {
+    // $this->withoutExceptionHandling();
+    $user = User::factory()->create();
+    $recourse = Recourse::factory()->create(["user_id" => $user->id]);
 
-        $recourse = Recourse::factory()->create();
+    $tags = Tag::factory(5)->create();
+    $recourse->tags()->syncWithoutDetaching($tags->pluck('id'));
 
-        $tags = Tag::factory(5)->create();
-        $recourse->tags()->syncWithoutDetaching($tags->pluck('id'));
+    $response = $this->actingAs($user)->getJson(route('recourses.show', $recourse->id));
+    // dd($response->getContent());
 
-        $response = $this->getJson(route('recourses.show', $recourse->id));
-        // dd($response->getContent());
+    $response->assertStatus(Response::HTTP_OK);
 
-        $response->assertStatus(Response::HTTP_OK);
+    $response->assertJsonStructure([
+      "data" => [
+        "nombre",
+        "ruta",
+        "autor",
+        "editorial",
+        "tipoId",
+        "totalPaginas",
+        "totalCapitulos",
+        "totalVideos",
+        "totalHoras",
+        "status",
+        "progress",
+        "tags"
+      ]
+    ]);
+  }
 
-        $response->assertJsonStructure([
-            "data" => [
-                "nombre",
-                "ruta",
-                "autor",
-                "editorial",
-                "tipoId",
-                "totalPaginas",
-                "totalCapitulos",
-                "totalVideos",
-                "totalHoras",
-                "status",
-                "progress",
-                "tags"
-            ]
-        ]);
-    }
+  /** @test */
+  public function get_detailed_data_of_recourse_without_tags()
+  {
+    // $this->withoutExceptionHandling();
 
-    /** @test */
-    public function get_detailed_data_of_recourse_without_tags()
-    {
-        // $this->withoutExceptionHandling();
+    $user = User::factory()->create();
+    $recourse = Recourse::factory()->create(["user_id" => $user->id]);
 
-        $recourse = Recourse::factory()->create();
+    $response = $this->actingAs($user)->getJson(route('recourses.show', $recourse->id));
 
-        $response = $this->getJson(route('recourses.show', $recourse->id));
+    $response->assertStatus(Response::HTTP_OK);
 
-        $response->assertStatus(Response::HTTP_OK);
+    // dd($response->getContent());
+    $response->assertJsonStructure([
+      "data" => [
+        "nombre",
+        "ruta",
+        "autor",
+        "editorial",
+        "tipoId",
+        "totalPaginas",
+        "totalCapitulos",
+        "totalVideos",
+        "totalHoras",
+        "status",
+        "progress",
+        "tags"
+      ]
+    ]);
+  }
 
-        // dd($response->getContent());
-        $response->assertJsonStructure([
-            "data" => [
-                "nombre",
-                "ruta",
-                "autor",
-                "editorial",
-                "tipoId",
-                "totalPaginas",
-                "totalCapitulos",
-                "totalVideos",
-                "totalHoras",
-                "status",
-                "progress",
-                "tags"
-            ]
-        ]);
-    }
-
-    //TODO Hacer el test en el caso de uso que un recourse este eliminado y se deniegue el acceso
+  //TODO Hacer el test en el caso de uso que un recourse este eliminado y se deniegue el acceso
 }
