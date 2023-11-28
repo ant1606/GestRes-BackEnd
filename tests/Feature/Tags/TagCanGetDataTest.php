43ee608c47d90ba598ef8b1a7fca8a806e81ee55
Anthony <anthony.tp1606@gmail.com>
2023-11-28 18:54:21 -0500

    fix test: Corrigiendo Tests de TagCanGetDataTest

--- tests/Feature/Tags/TagCanGetDataTest.php
+++ tests/Feature/Tags/TagCanGetDataTest.php
@@ -3,100 +3,102 @@
 namespace Tests\Feature\Tags;
 
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class TagCanGetDataTest extends TestCase
 {
-    use RefreshDatabase;
+  use RefreshDatabase;
 
-    /** @test */
-    public function can_get_list_of_tags()
-    {
-        $this->withoutExceptionHandling();
+  /** @test */
+  public function can_get_list_of_tags()
+  {
+    // $this->withoutExceptionHandling();
+    $user = User::factory()->create();
 
-        Tag::Factory(10)->create();
+    Tag::Factory(10)->create();
 
-        $response = $this->getJson(route('tag.index'));
+    $response = $this->actingAs($user)->getJson(route('tag.index'));
 
-        $response->assertStatus(Response::HTTP_ACCEPTED);
-        // dd($response);
-        $response->assertJsonStructure([
-            "data" => [
-                [
-                    "identificador",
-                    "nombre",
-                    "estilos",
-                ]
-            ]
-        ]);
-        // dd($tags);
-    }
+    $response->assertStatus(Response::HTTP_ACCEPTED);
+    // dd($response);
+    $response->assertJsonStructure([
+      "data" => [
+        [
+          "identificador",
+          "nombre",
+          "estilos",
+        ]
+      ]
+    ]);
+    // dd($tags);
+  }
 
-    /** @test */
-    public function can_search_tags_with_minimun_length_of_query_value_is_equals_or_higher_than_2()
-    {
-        $this->withoutExceptionHandling();
+  /** @test */
+  public function can_search_tags_with_minimun_length_of_query_value_is_equals_or_higher_than_2()
+  {
+    // $this->withoutExceptionHandling();
+    $user = User::factory()->create();
+    Tag::Factory(10)->create();
+    $filter = "MI ETIQUETA";
+    Tag::create(['name' => "MI ETIQUETA"]);
 
-        Tag::Factory(10)->create();
-        $filter = "MI ETIQUETA";
-        Tag::create(['name' => "MI ETIQUETA"]);
+    $response = $this->actingAs($user)->getJson(route('tag.index',  ["searchNombre" => $filter]));
 
-        $response = $this->getJson(route('tag.index',  ["searchNombre" => $filter]));
+    //         dd($response->getContent());
+    $response->assertStatus(Response::HTTP_ACCEPTED);
+    $response->assertJsonStructure([
+      "data" => [
+        [
+          "identificador",
+          "nombre",
+          "estilos",
+        ]
+      ]
+    ]);
+    //        $response->assertJsonCount(1);
+    // dd($tags);
+  }
 
-//         dd($response->getContent());
-        $response->assertStatus(Response::HTTP_ACCEPTED);
-        $response->assertJsonStructure([
-            "data" => [
-                [
-                    "identificador",
-                    "nombre",
-                    "estilos",
-                ]
-            ]
-        ]);
-//        $response->assertJsonCount(1);
-        // dd($tags);
-    }
-
-    /** @test */
-    public function get_response_when_does_not_found_results_from_filter()
-    {
-        $this->withoutExceptionHandling();
-
-        Tag::Factory(10)->create();
-        $filter = "asdasd";
+  /** @test */
+  public function get_response_when_does_not_found_results_from_filter()
+  {
+    // $this->withoutExceptionHandling();
+    $user = User::factory()->create();
+    Tag::Factory(10)->create();
+    $filter = "asdasd";
 
 
-        $response = $this->getJson(route('tag.index', ["searchNombre" => $filter]));
+    $response = $this->actingAs($user)->getJson(route('tag.index', ["searchNombre" => $filter]));
 
-        // Cuando se tiene un codigo de respuesta 204, no devuelve un body,
-        // $response->assertStatus(Response::HTTP_NO_CONTENT);
-        $response->assertStatus(Response::HTTP_OK);
-        $response->assertJsonStructure([
-            'data',
-            'message'
-        ]);
-    }
+    // Cuando se tiene un codigo de respuesta 204, no devuelve un body,
+    // $response->assertStatus(Response::HTTP_NO_CONTENT);
+    $response->assertStatus(Response::HTTP_OK);
+    $response->assertJsonStructure([
+      'data',
+      'message'
+    ]);
+  }
 
-    // /** @test */
-    // public function can_not_get_results_when_filter_is_less_or_equal_to_2()
-    // {
-    //     $this->withoutExceptionHandling();
+  // /** @test */
+  // public function can_not_get_results_when_filter_is_less_or_equal_to_2()
+  // {
+  //     $this->withoutExceptionHandling();
 
-    //     Tag::Factory(10)->create();
-    //     $filter = "Mi";
-    //     $tag = Tag::create(['name' => "Mi Etiqueta"]);
+  //     Tag::Factory(10)->create();
+  //     $filter = "Mi";
+  //     $tag = Tag::create(['name' => "Mi Etiqueta"]);
 
-    //     $response = $this->getJson(route('tag.index', ["filter" => $filter]));
+  //     $response = $this->getJson(route('tag.index', ["filter" => $filter]));
 
-    //     $response->assertStatus(Response::HTTP_LENGTH_REQUIRED);
-    //     $response->assertJsonStructure([
-    //         'error',
-    //         'code'
-    //     ]);
-    //     // $response->assertJsonCount(1);
-    //     // dd($tags);
-    // }
+  //     $response->assertStatus(Response::HTTP_LENGTH_REQUIRED);
+  //     $response->assertJsonStructure([
+  //         'error',
+  //         'code'
+  //     ]);
+  //     // $response->assertJsonCount(1);
+  //     // dd($tags);
+  // }
 }
